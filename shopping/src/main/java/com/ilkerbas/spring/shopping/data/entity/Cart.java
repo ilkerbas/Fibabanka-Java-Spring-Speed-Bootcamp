31802f6d5aa4e89b570cdb22daad487c0213e8e5
ilkerbas <bas.ilker@outlook.com>
2022-11-08 23:45:21 +0300

    Defining entity classes

--- shopping/src/main/java/com/ilkerbas/spring/shopping/data/entity/Cart.java
+++ shopping/src/main/java/com/ilkerbas/spring/shopping/data/entity/Cart.java
@@ -1,5 +1,75 @@
 package com.ilkerbas.spring.shopping.data.entity;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+//Cart Entity
+
+@Entity
 public class Cart {
 
+	// fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long cartId;
+	
+	private String customerName;
+	
+	private long totalAmount;
+	
+	private boolean cartStatus;
+	
+	@OneToMany(mappedBy = "cart")
+	private List<CartProduct> cartProducts;
+
+	// NoArgs constructor
+	public Cart() {
+	}
+
+	// AllArgs constructor
+	public Cart(long cartId, String customerName, long totalAmount, boolean cartStatus) {
+		this.cartId = cartId;
+		this.customerName = customerName;
+		this.totalAmount = totalAmount;
+		this.cartStatus = cartStatus;
+	}
+
+	// getters & setters
+	public long getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(long cartId) {
+		this.cartId = cartId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public long getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(long totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public boolean isCartStatus() {
+		return cartStatus;
+	}
+
+	public void setCartStatus(boolean cartStatus) {
+		this.cartStatus = cartStatus;
+	}
+	
 }
--- shopping/src/main/java/com/ilkerbas/spring/shopping/data/entity/CartProduct.java
+++ shopping/src/main/java/com/ilkerbas/spring/shopping/data/entity/CartProduct.java
@@ -1,5 +1,95 @@
 package com.ilkerbas.spring.shopping.data.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+//CartProduct Entity
+
+@Entity
 public class CartProduct {
 
+	// fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long cartProductId;
+	
+	@ManyToOne
+	@JoinColumn(name = "cart_id")
+	private Cart cart;
+	
+	private long productId;
+	
+	private int salesQuantity;
+	
+	private long salesPrice;
+	
+	private int lineAmount;
+
+	// AllArgs constructor
+	public CartProduct(long cartProductId, long productId, int salesQuantity, long salesPrice,
+			int lineAmount) {
+		this.cartProductId = cartProductId;
+		this.productId = productId;
+		this.salesQuantity = salesQuantity;
+		this.salesPrice = salesPrice;
+		this.lineAmount = lineAmount;
+	}
+
+	// NoArgs constructor
+	public CartProduct() {
+	}
+
+	// getters & setters
+	public long getCartProductId() {
+		return cartProductId;
+	}
+
+	public void setCartProductId(long cartProductId) {
+		this.cartProductId = cartProductId;
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+
+	public long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+
+	public int getSalesQuantity() {
+		return salesQuantity;
+	}
+
+	public void setSalesQuantity(int salesQuantity) {
+		this.salesQuantity = salesQuantity;
+	}
+
+	public long getSalesPrice() {
+		return salesPrice;
+	}
+
+	public void setSalesPrice(long salesPrice) {
+		this.salesPrice = salesPrice;
+	}
+
+	public int getLineAmount() {
+		return lineAmount;
+	}
+
+	public void setLineAmount(int lineAmount) {
+		this.lineAmount = lineAmount;
+	}
+	
 }
