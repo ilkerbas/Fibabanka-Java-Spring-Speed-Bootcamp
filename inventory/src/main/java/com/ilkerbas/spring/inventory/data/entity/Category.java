6c68d7b2fda9d7b2270af573f0b5aada3e5da67e
ilkerbas <bas.ilker@outlook.com>
2022-11-08 18:57:59 +0300

    Defining entity classes

--- inventory/src/main/java/com/ilkerbas/spring/inventory/data/entity/Category.java
+++ inventory/src/main/java/com/ilkerbas/spring/inventory/data/entity/Category.java
@@ -1,8 +1,52 @@
 package com.ilkerbas.spring.inventory.data.entity;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 // Category Entity
 
+@Entity
 public class Category {
 
+	// fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long categoryId;
+	
+	private String categoryName;
+	
+	@OneToMany(mappedBy = "category")
+	private List<Product> products;
+
+	// default constructor
+	public Category() {}
+
+	// constructor with fields
+	public Category(long categoryId, String categoryName) {
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
+
+	// getters & setters
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
 }
--- inventory/src/main/java/com/ilkerbas/spring/inventory/data/entity/Product.java
+++ inventory/src/main/java/com/ilkerbas/spring/inventory/data/entity/Product.java
@@ -1,8 +1,71 @@
 package com.ilkerbas.spring.inventory.data.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 // Product Entity
 
+@Entity
 public class Product {
 
+	// fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long productId;
+	
+	private String productName;
+	
+	private double salesPrice;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	// default constructor
+	public Product() {}
+
+	// constructor with fields
+	public Product(long productId, String productName, double salesPrice) {
+		this.productId = productId;
+		this.productName = productName;
+		this.salesPrice = salesPrice;
+	}
+
+	// getters & setters
+	public long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getSalesPrice() {
+		return salesPrice;
+	}
+
+	public void setSalesPrice(double salesPrice) {
+		this.salesPrice = salesPrice;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
 }
