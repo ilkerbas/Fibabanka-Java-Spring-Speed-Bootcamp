142a4c1289bab1f8f0434c38fb66453a8cd9cbcf
ilkerbas <bas.ilker@outlook.com>
2022-11-08 19:48:36 +0300

    Defining repositories

--- inventory/src/main/java/com/ilkerbas/spring/inventory/data/repository/CategoryRepository.java
+++ inventory/src/main/java/com/ilkerbas/spring/inventory/data/repository/CategoryRepository.java
@@ -1,11 +1,15 @@
 package com.ilkerbas.spring.inventory.data.repository;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.ilkerbas.spring.inventory.data.entity.Category;
+
 /* Category Repository
  * Takes Entity
  * SQL query, works with database
  * CRUD operations and custom queries
  */
 
-public class CategoryRepository {
+public interface CategoryRepository extends CrudRepository<Category, Long> {
 
 }
--- inventory/src/main/java/com/ilkerbas/spring/inventory/data/repository/ProductRepository.java
+++ inventory/src/main/java/com/ilkerbas/spring/inventory/data/repository/ProductRepository.java
@@ -1,5 +1,12 @@
 package com.ilkerbas.spring.inventory.data.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.ilkerbas.spring.inventory.data.entity.Product;
 
 /* Product Repository
  * Takes Entity
@@ -7,6 +14,9 @@ package com.ilkerbas.spring.inventory.data.repository;
  * CRUD operations and custom queries
  */
 
-public class ProductRepository {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 
+	// query for getting all products by given category id
+	@Query("select p from Product p where p.category.categoryId = :categoryId")
+	List<Product> findAllByCategoryId(@Param("categoryId") long categoryId);
 }
