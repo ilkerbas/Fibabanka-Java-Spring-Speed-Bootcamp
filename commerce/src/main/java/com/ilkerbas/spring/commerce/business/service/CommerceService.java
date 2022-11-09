44e65ce9ae54eeaa8f81d542afd9a9f41474c41c
ilkerbas <bas.ilker@outlook.com>
2022-11-09 21:09:16 +0300

    Service implementation for Commerce app

--- commerce/src/main/java/com/ilkerbas/spring/commerce/business/service/CommerceService.java
+++ commerce/src/main/java/com/ilkerbas/spring/commerce/business/service/CommerceService.java
@@ -1,5 +1,36 @@
 package com.ilkerbas.spring.commerce.business.service;
 
+import java.util.List;
+
+import com.ilkerbas.spring.commerce.business.dto.CartDto;
+import com.ilkerbas.spring.commerce.business.dto.CartProductDto;
+import com.ilkerbas.spring.commerce.business.dto.CategoryDto;
+import com.ilkerbas.spring.commerce.business.dto.ProductDto;
+
 public interface CommerceService {
 
+	// get all categories
+	List<CategoryDto> findAllCategories();
+	
+	// read product by id
+	ProductDto findProduct(long productId);
+	
+	// read products by categoryId
+	List<ProductDto> findAllByCategoryId(long categoryId);
+	
+	// delete cart product
+	void deleteCartProduct(long cartId, long productId);
+	
+	// create with given cart create DTO
+	// return cartId
+	long create(CartDto cartDto);
+	
+	// add cartProduct to cart
+	CartProductDto addProduct(CartProductDto cartProductDto);
+	
+	// find cart by given id
+	CartDto find(long cartId);
+	
+	// checkout
+	boolean checkout(long cartId);
 }
--- commerce/src/main/java/com/ilkerbas/spring/commerce/business/service/CommerceServiceImpl.java
+++ commerce/src/main/java/com/ilkerbas/spring/commerce/business/service/CommerceServiceImpl.java
@@ -1,5 +1,91 @@
 package com.ilkerbas.spring.commerce.business.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ilkerbas.spring.commerce.business.dto.CartDto;
+import com.ilkerbas.spring.commerce.business.dto.CartProductDto;
+import com.ilkerbas.spring.commerce.business.dto.CategoryDto;
+import com.ilkerbas.spring.commerce.business.dto.ProductDto;
+import com.ilkerbas.spring.commerce.presentation.client.InventoryClient;
+import com.ilkerbas.spring.commerce.presentation.client.ShoppingClient;
+
 public class CommerceServiceImpl implements CommerceService {
+
+	@Autowired
+	private InventoryClient inventoryClient;
+	
+	@Autowired
+	private ShoppingClient shoppingClient;
+	
+	
+	// get all categories
+	@Override
+	public List<CategoryDto> findAllCategories() {
+		
+		List<CategoryDto> categoryDtos = inventoryClient.findAllCategories();
+		
+		return categoryDtos;
+	}
+
+	// read product by id
+	@Override
+	public ProductDto findProduct(long productId) {
+
+		ProductDto productDto = inventoryClient.findProduct(productId);
+		
+		return productDto;
+	}
+
+	// read products by categoryId
+	@Override
+	public List<ProductDto> findAllByCategoryId(long categoryId) {
+
+		List<ProductDto> productDtos = inventoryClient.findAllByCategoryId(categoryId);
+		
+		return productDtos;
+	}
+
+	// delete cart product
+	@Override
+	public void deleteCartProduct(long cartId, long productId) {
+		
+		shoppingClient.deleteCartProduct(cartId, productId);
+		
+	}
+
+	// create with given cart create DTO
+	// return cartId
+	@Override
+	public long create(CartDto cartDto) {
+		
+		long cartId = shoppingClient.create(cartDto);
+		
+		return cartId;
+	}
+
+	// add cartProduct to cart
+	@Override
+	public CartProductDto addProduct(CartProductDto cartProductDto) {
+		
+		return shoppingClient.addProduct(cartProductDto);
+	}
+
+	// find cart by given id
+	@Override
+	public CartDto find(long cartId) {
+
+		CartDto cartDto = shoppingClient.find(cartId);
+		
+		return cartDto;
+	}
+
+	// checkout
+	@Override
+	public boolean checkout(long cartId) {
+		
+		return shoppingClient.checkout(cartId);
+	}
 
 }
