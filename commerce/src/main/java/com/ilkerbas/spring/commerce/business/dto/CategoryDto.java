9a62dc519d3955dac5007ba64d80a8b672d2bb24
ilkerbas <bas.ilker@outlook.com>
2022-11-09 20:09:13 +0300

    Defining DTOs

--- commerce/src/main/java/com/ilkerbas/spring/commerce/business/dto/CategoryDto.java
+++ commerce/src/main/java/com/ilkerbas/spring/commerce/business/dto/CategoryDto.java
@@ -1,5 +1,39 @@
 package com.ilkerbas.spring.commerce.business.dto;
 
+// Category Data Transfer Object
 public class CategoryDto {
+
+	// fields
+	private long categoryId;
+
+	private String categoryName;
+
+	// no-argument constructor
+	public CategoryDto() {
+
+	}
+
+	// AllArgsConstructor
+	public CategoryDto(long categoryId, String categoryName) {
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
+
+	// getters & setters
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryId(long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
 
 }
--- commerce/src/main/java/com/ilkerbas/spring/commerce/business/dto/ProductDto.java
+++ commerce/src/main/java/com/ilkerbas/spring/commerce/business/dto/ProductDto.java
@@ -1,5 +1,43 @@
 package com.ilkerbas.spring.commerce.business.dto;
 
+//Product data transfer object
 public class ProductDto {
 
+	// fields
+	private long productId;
+
+	private String productName;
+
+	private double salesPrice;
+
+	private long categoryId;
+
+	// NoArgsConstructor
+	public ProductDto() {
+	}
+
+	// AllArgsConstructor
+	public ProductDto(long productId, String productName, double salesPrice, long categoryId) {
+		this.productId = productId;
+		this.productName = productName;
+		this.salesPrice = salesPrice;
+		this.categoryId = categoryId;
+	}
+
+	// getters
+	public long getProductId() {
+		return productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public double getSalesPrice() {
+		return salesPrice;
+	}
+
+	public long getCategoryId() {
+		return categoryId;
+	}
 }
