e6b37b4732bd59bc5e83522d202b9845f9fd7b2b
ilkerbas <bas.ilker@outlook.com>
2022-11-09 21:05:49 +0300

    Defining clients - Components

--- commerce/src/main/java/com/ilkerbas/spring/commerce/presentation/client/InventoryClient.java
+++ commerce/src/main/java/com/ilkerbas/spring/commerce/presentation/client/InventoryClient.java
@@ -1,5 +1,53 @@
 package com.ilkerbas.spring.commerce.presentation.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.ilkerbas.spring.commerce.business.dto.CategoryDto;
+import com.ilkerbas.spring.commerce.business.dto.ProductDto;
+
+// Client for inventory MicroService
+// Communicates with RestTemplate
+@Component
 public class InventoryClient {
 
+	// find all categories
+	public List<CategoryDto> findAllCategories() {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String url = "http://localhost:8081/inventory/categories/";
+		
+		@SuppressWarnings("unchecked")
+		ArrayList<CategoryDto> categories = restTemplate.getForObject(url, ArrayList.class);
+		
+		return categories;
+	}
+
+	// find product by given id
+	public ProductDto findProduct(long productId) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String url = "http://localhost:8081/inventory/product/" + productId;
+		
+		ProductDto productDto = restTemplate.getForObject(url, ProductDto.class);
+		return productDto;
+	}
+
+	// find all products by given category id
+	public List<ProductDto> findAllByCategoryId(long categoryId) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String url = "http://localhost:8081/inventory/products/" + categoryId;
+		
+		@SuppressWarnings("unchecked")
+		List<ProductDto> productDtos = restTemplate.getForObject(url, ArrayList.class);
+		
+		return productDtos;
+	}
 }
--- commerce/src/main/java/com/ilkerbas/spring/commerce/presentation/client/ShoppingClient.java
+++ commerce/src/main/java/com/ilkerbas/spring/commerce/presentation/client/ShoppingClient.java
@@ -1,5 +1,74 @@
 package com.ilkerbas.spring.commerce.presentation.client;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.ilkerbas.spring.commerce.business.dto.CartDto;
+import com.ilkerbas.spring.commerce.business.dto.CartProductDto;
+
+
+//Client for shopping MicroService
+//Communicates with RestTemplate
+@Component
 public class ShoppingClient {
 
+	// delete cart product by given cartId and productId
+	public void deleteCartProduct(long cartId, long productId) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String url = "http://localhost:8082/shopping/cart/"
+				+ cartId + "/remove/" + productId;
+		
+		restTemplate.delete(url);
+		
+	}
+
+	// create a cart
+	public long create(CartDto cartDto) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String url = "http://localhost:8082/shopping/cart/create";
+		
+		Long cartId = restTemplate.postForObject(url, cartDto, Long.class);
+		
+		return cartId;
+	}
+
+	// add product to cart
+	public CartProductDto addProduct(CartProductDto cartProductDto) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String url = "http://localhost:8082/shopping/cart/add";
+		
+		CartProductDto cartProduct =  restTemplate.postForObject(url, cartProductDto, CartProductDto.class);
+		
+		return cartProduct;
+	}
+
+	// find cart by id
+	public CartDto find(long cartId) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String url = "http://localhost:8082/shopping/cart/find/" + cartId;
+		
+		CartDto cartDto = restTemplate.getForObject(url, CartDto.class);
+		
+		return cartDto;
+	}
+
+	// checkout
+	public boolean checkout(long cartId) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String url = "http://localhost:8082/shopping/checkout/" + cartId;
+		
+		boolean result = restTemplate.getForObject(url, Boolean.class);
+		
+		return result;
+	}
 }
