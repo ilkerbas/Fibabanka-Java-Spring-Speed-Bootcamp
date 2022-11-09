b736a86772fdbc10367534e752e2f278b7dc28fb
ilkerbas <bas.ilker@outlook.com>
2022-11-09 21:35:41 +0300

    missing dtos defined

--- commerce/src/main/java/com/ilkerbas/spring/commerce/business/dto/CartDto.java
+++ commerce/src/main/java/com/ilkerbas/spring/commerce/business/dto/CartDto.java
@@ -1,5 +1,74 @@
 package com.ilkerbas.spring.commerce.business.dto;
 
+import java.util.List;
+
+
 public class CartDto {
 
+	// fields
+		private long cartId;
+		
+		private String customerName;
+		
+		private long totalAmount;
+		
+		private boolean cartStatus;
+		
+		private List<CartProductDto> cartProductDtos;
+		
+		// NoArgs Constructor
+		public CartDto() {
+			
+		}
+
+		// AllArgs Constructor
+		public CartDto(long cartId, String customerName, long totalAmount, boolean cartStatus,
+				List<CartProductDto> cartProductDtos) {
+			this.cartId = cartId;
+			this.customerName = customerName;
+			this.totalAmount = totalAmount;
+			this.cartStatus = cartStatus;
+			this.cartProductDtos = cartProductDtos;
+		}
+		
+		// getters & setters
+		public long getCartId() {
+			return cartId;
+		}
+
+		public void setCartId(long cartId) {
+			this.cartId = cartId;
+		}
+
+		public String getCustomerName() {
+			return customerName;
+		}
+
+		public void setCustomerName(String customerName) {
+			this.customerName = customerName;
+		}
+
+		public long getTotalAmount() {
+			return totalAmount;
+		}
+
+		public void setTotalAmount(long totalAmount) {
+			this.totalAmount = totalAmount;
+		}
+
+		public boolean isCartStatus() {
+			return cartStatus;
+		}
+
+		public void setCartStatus(boolean cartStatus) {
+			this.cartStatus = cartStatus;
+		}
+
+		public List<CartProductDto> getCartProductDtos() {
+			return cartProductDtos;
+		}
+
+		public void setCartProductDtos(List<CartProductDto> cartProductDtos) {
+			this.cartProductDtos = cartProductDtos;
+		}
 }
--- commerce/src/main/java/com/ilkerbas/spring/commerce/business/dto/CartProductDto.java
+++ commerce/src/main/java/com/ilkerbas/spring/commerce/business/dto/CartProductDto.java
@@ -2,4 +2,80 @@ package com.ilkerbas.spring.commerce.business.dto;
 
 public class CartProductDto {
 
+	// fields 
+		private long cartProductId;
+		
+		private long productId;
+		
+		private int salesQuantity;
+		
+		private long salesPrice;
+		
+		private int lineAmount;
+		
+		private long cartId;
+		
+		// NoArgs Constructor
+		public CartProductDto() {
+		}
+
+		// FullArgs Constructor
+		public CartProductDto(long cartProductId, long productId, int salesQuantity, long salesPrice, int lineAmount,
+				long cartId) {
+			this.cartProductId = cartProductId;
+			this.productId = productId;
+			this.salesQuantity = salesQuantity;
+			this.salesPrice = salesPrice;
+			this.lineAmount = lineAmount;
+			this.cartId = cartId;
+		}
+
+		// getters & setters
+		public long getCartProductId() {
+			return cartProductId;
+		}
+
+		public void setCartProductId(long cartProductId) {
+			this.cartProductId = cartProductId;
+		}
+
+		public long getProductId() {
+			return productId;
+		}
+
+		public void setProductId(long productId) {
+			this.productId = productId;
+		}
+
+		public int getSalesQuantity() {
+			return salesQuantity;
+		}
+
+		public void setSalesQuantity(int salesQuantity) {
+			this.salesQuantity = salesQuantity;
+		}
+
+		public long getSalesPrice() {
+			return salesPrice;
+		}
+
+		public void setSalesPrice(long salesPrice) {
+			this.salesPrice = salesPrice;
+		}
+
+		public int getLineAmount() {
+			return lineAmount;
+		}
+
+		public void setLineAmount(int lineAmount) {
+			this.lineAmount = lineAmount;
+		}
+
+		public long getCartId() {
+			return cartId;
+		}
+
+		public void setCartId(long cartId) {
+			this.cartId = cartId;
+		}
 }
